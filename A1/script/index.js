0ee49bd9104e4603767cbce0b1ce91a8e9cd156b
Your Name <tiwihi@hanmail.net>
2024-10-28 12:58:08 +0900

    a-1.1

--- A1/script/index.js
+++
@@ -1,7 +0,0 @@
-$('li.mainmenu').mouseenter(function () {
-    $(this).find('.submenu').stop().slideDown(1000)
-}
-)
-$('li.mainmenu').mouseleave(function(){
-    $(this).find('.submenu').stop().slideUp()
-})
--- A2/JavaScript/index.js
+++
@@ -1,8 +0,0 @@
-$('li.mainmenu').mouseenter(function(){
-    $('.submenu').stop().slideDown(400)
-    $('header').addClass('on')
-})
-$('li.mainmenu').mouseleave(function(){
-    $('.submenu').stop().slideUp(400)
-    $('header').removeClass('on')
-})
